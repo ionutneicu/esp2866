f07a43e2bdc8b9a3eda7f5d07c70787a83c1bc05
Ionut Neicu <ionut.neicu@radcom.com>
2019-11-02 18:04:53 +0200

    added burn device no OTA

---
+++ scripts/burn_flash.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+
+#sudo python esptool.py --port /dev/ttyUSB0  erase_flash
+#sudo python esptool.py --port /dev/ttyUSB0  write_flash 0x3fc000 esp_init_data_default.bin
+#sudo python esptool.py --port /dev/ttyUSB0  write_flash 0x3fc000 ../../bins/esp_init_data_default.bin
+#sudo python esptool.py --port /dev/ttyUSB0  write_flash 0x3fe000 ../../bins/blank.bin
+sudo python esptool.py --port /dev/ttyUSB0  write_flash 0x00000 ../../esp8266_bin/eagle.flash.bin
+sudo python esptool.py --port /dev/ttyUSB0  write_flash 0x20000 ../../esp8266_bin/eagle.irom0text.bin
