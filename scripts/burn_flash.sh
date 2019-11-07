b159f6beaa3fef843f55f29f7fec309c8e97d21c
Ionut Neicu <ionut.neicu@radcom.com>
2019-11-07 14:45:23 +0200

    fixed buring flash scripts

--- scripts/burn_flash.sh
+++ scripts/burn_flash.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 
-#sudo python esptool.py --port /dev/ttyUSB0  erase_flash
-#sudo python esptool.py --port /dev/ttyUSB0  write_flash 0x3fc000 esp_init_data_default.bin
-#sudo python esptool.py --port /dev/ttyUSB0  write_flash 0x3fc000 ../../bins/esp_init_data_default.bin
-#sudo python esptool.py --port /dev/ttyUSB0  write_flash 0x3fe000 ../../bins/blank.bin
-sudo python ${ESPTOOL_PATH}/esptool.py --port /dev/ttyUSB0  write_flash 0x00000 ../../esp8266_bin/eagle.flash.bin
-sudo python ${ESPTOOL_PATH}/esptool.py --port /dev/ttyUSB0  write_flash 0x20000 ../../esp8266_bin/eagle.irom0text.bin
+DEVICE=/dev/ttyUSB0
+
+#sudo python ${ESPTOOL_PATH}/esptool.py --port ${DEVICE}  erase_flash
+#sudo python ${ESPTOOL_PATH}/esptool.py --port ${DEVICE}  write_flash 0x3fc000 esp_init_data_default.bin
+#sudo python ${ESPTOOL_PATH}/esptool.py --port ${DEVICE}  write_flash 0x3fc000 ../../bins/esp_init_data_default.bin
+#sudo python ${ESPTOOL_PATH}/esptool.py --port ${DEVICE}  write_flash 0x3fe000 ../../bins/blank.bin
+sudo python ${ESPTOOL_PATH}/esptool.py --port ${DEVICE}  write_flash 0x00000  ../../esp8266_bin/eagle.flash.bin
+sudo python ${ESPTOOL_PATH}/esptool.py --port ${DEVICE}  write_flash 0x20000  ../../esp8266_bin/eagle.irom0text.bin
