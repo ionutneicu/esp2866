3cfd76f591b98503d2b29ad61c528802eab1296e
Ionut Neicu <ionut.neicu@gmail.com>
2019-11-07 14:18:26 +0200

    Updated env.sh - removed dev values
    dev values to be added by install script

--- env.sh
+++ env.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
-#Added above by install.sh script:
-export PATH="${PATH}:/home/ionut/work/esp8266/xtensa-lx106-elf/bin"
-export SDK_PATH="/home/ionut/work/esp8266/ESP8266_RTOS_SDK"
-export BIN_PATH="/home/ionut/work/esp8266/esp8266_bin"
-export ESPTOOL_PATH="/home/ionut/work/esp8266/esptool"
+# install.sh script:
+
